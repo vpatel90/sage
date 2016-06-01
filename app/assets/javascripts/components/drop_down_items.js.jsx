e0f3f5ac9f4eea5f14d4109e73807ba1c3522efa
Vivek Patel <vnp229@gmail.com>
2016-06-01 16:21:58 -0500

    added a toast notification when trying to add the same item twice

--- app/assets/javascripts/components/drop_down_items.js.jsx
+++ app/assets/javascripts/components/drop_down_items.js.jsx
@@ -1,12 +1,10 @@
 var DropDownItems = React.createClass({
   handleClick: function(){
     var itemBox = document.getElementById('itemsInBox');
-    console.log(this.props.selectedItems)
     if (_.contains(this.props.selectedItems, this.props.name)){
-
+      Materialize.toast("Item already on list", 2000)
     }else {
       this.props.addSelectedItems(this.props.name);
-
       itemBox.innerHTML = itemBox.innerHTML + '<p>' + this.props.name + '</p>';
     }
   },
