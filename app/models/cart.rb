33d9e5a3542274a8435a53be5e71e89ebf96afc2
Vivek Patel <vnp229@gmail.com>
2016-06-01 23:55:32 -0500

    added through relations for polymorphics in cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,7 @@
 class Cart < ActiveRecord::Base
   belongs_to :user
   has_many :cart_items
+  has_many :sample_boxes, through: :cart_items, source: :cart_itemable, source_type: "SampleBox"
+  has_many :items, through: :cart_items, source: :cart_itemable, source_type: "Item"
+
 end
